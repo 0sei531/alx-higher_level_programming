baa57874f796746d09385811b639d4e12caf97f6
0sei531 <oseikofiantwi531@gmail.com>
2023-11-30 17:16:02 -0800

    lyn

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+
+from sys import argv, exit as sys_exit
+
 from calculator_1 import add, sub, mul, div
-from sys import argv
-
-
-def calculate(a, operator, b):
-    if operator == "+":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, add(a, b)))
-    elif operator == "-":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, sub(a, b)))
-    elif operator == "*":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, mul(a, b)))
-    elif operator == "/":
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
 
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    else:
-        a = int(argv[1])
-        operator = argv[2]
-        b = int(argv[3])
-        calculate(a, oper
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    try:
+        a, op, b = argv[1:]
+
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys_exit(1)
+
+    try:
+        print(a, op, b, "=", ops[op](int(a), int(b)))
+
+    except KeyError:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys_exit(1)
+
+    except ValueError:
+        print("Invalid operand. Must be an integer")
+        sys_exit(1)
+
+    except ZeroDivisionError:
+        print("Invalid operation. Cannot divide by zero")
+        sys_exit(1)
