66effbd71870ae48ba2faa223158e86e3c690dde
0sei531 <oseikofiantwi531@gmail.com>
2023-12-07 05:01:24 -0800

    lyn

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
@@ -9,4 +8,3 @@ def max_integer(my_list=[]):
             if i > temp:
                 temp = i
         return temp
-
