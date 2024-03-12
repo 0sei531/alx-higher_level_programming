a51acac2e18c4ed994325eee1963969b1fc4426c
0sei531 <oseikofiantwi531@gmail.com>
2024-03-11 18:25:26 -0700

    a

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
+// executes x times a function.
 
-// Define a function that increments a number and calls another function
-function incrementAndCall(number, theFunction) {
-    // Increment the number
-    number++;
-    // Call theFunction with the incremented number as argument
-    theFunction(number);
-}
-
-// Export the function to make it visible from outside
-module.exports.incrementAndCall = incrementAndCall;
+exports.addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
 
