2db886d9032ea9740e8cd4effb2fa9914be094fd
0sei531 <oseikofiantwi531@gmail.com>
2024-03-27 11:53:45 -0700

    k

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,29 @@
-#!/usr/bin/python3
-# Defines a peak-finding algorithm.
+# Correcting block comment
+#!/usr/bin/env python3
+"""
+A peak in a list is a value that is greater than or equal to its neighbors.
+"""
+
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """
+    Function to find a peak in a list of integers.
+    """
+    if not list_of_integers:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
+    n = len(list_of_integers)
+    if n == 1:
         return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    start, end = 0, n - 1
+
+    while start < end:
+        mid = (start + end) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            start = mid + 1
+        else:
+            end = mid
+
+    return list_of_integers[start]
+
