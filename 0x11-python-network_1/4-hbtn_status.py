c1c7fea39c58db6f48d853c13bb8d63a1a972f68
0sei531 <oseikofiantwi531@gmail.com>
2024-03-28 15:41:42 -0700

    far

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+
+def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
+    """
+    Send a GET request to the url
+    and print the response
+    """
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
+
 
 if __name__ == "__main__":
-    request = urllib.request.Request(
-        "https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    get_alx_intranet()
